4b9d705b1a84d6ad82748031815d6fc974b5a9c6
Carvalho, Marcelo <e068805@mastercard.com>
2019-02-20 14:46:52 -0300

    Create Project

---
+++ src/main/java/com/mvalho/supermarketlist/SupermarketListApplication.java
@@ -0,0 +1,13 @@
+package com.mvalho.supermarketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketListApplication.class, args);
+	}
+
+}
